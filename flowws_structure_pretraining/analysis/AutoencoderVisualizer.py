d6448c04a3a234e9051bf5f5eb14e9d9260a09fd
Matthew Spellings <mspells@vectorinstitute.ai>
2022-02-13 10:31:36 -0500

    Update AutoencoderVisualizer to use Plato for viz and support generators

--- flowws_structure_pretraining/analysis/AutoencoderVisualizer.py
+++ flowws_structure_pretraining/analysis/AutoencoderVisualizer.py
@@ -1,5 +1,8 @@
+import functools
+
 import flowws
 from flowws import Argument as Arg
+import numpy as np
 from plato import draw
 
 
@@ -13,38 +16,55 @@ class AutoencoderVisualizer(flowws.Stage):
         Arg(
             'true_color',
             '-t',
-            [float],
-            (0.5, 0.1, 0.1, 0.5),
+            (float, float, float, float),
             help='Color to use for the true positions',
         ),
         Arg(
             'prediction_color',
             '-p',
-            [float],
-            (0.1, 0.1, 0.5, 0.5),
+            (float, float, float, float),
             help='Color to use for the predicted positions',
         ),
     ]
 
     def run(self, scope, storage):
+        self.scope = scope
+
+        x, y = self.data_source
+
         self.arg_specifications['frame'].valid_values = flowws.Range(
-            0, len(scope['x_train'][0]), (True, False)
+            0, len(x[0]), (True, False)
+        )
+        true_color = self.arguments.get('true_color', None) or (0.5, 0.1, 0.1, 0.5)
+        prediction_color = self.arguments.get('prediction_color', None) or (
+            0.1,
+            0.1,
+            0.5,
+            0.5,
         )
 
         s = slice(self.arguments['frame'], self.arguments['frame'] + 1)
-        pred = scope['model'].predict([x[s] for x in scope['x_train']])
-        self.prediction = pred
-        self.trueval = scope['y_train'][s]
-        scope.setdefault('visuals', []).append(self)
+        pred = scope['model'].predict([v[s] for v in x])
 
-    def draw_plato(self):
-        trueprim = draw.Spheres(
-            positions=self.trueval[0], colors=self.arguments['true_color']
-        )
-        predprim = draw.Spheres(
-            positions=self.prediction[0], colors=self.arguments['prediction_color']
-        )
-        scene = draw.Scene(
-            [trueprim, predprim], zoom=15, features=dict(translucency=True)
-        )
-        return scene
+        prediction = pred[0] * scope['x_scale']
+        trueval = y[s][0] * scope['x_scale']
+
+        colors = np.repeat([prediction_color, true_color], len(trueval), axis=0)
+        scope['color'] = colors
+        scope['position'] = np.concatenate([prediction, trueval], axis=0)
+
+        if 'train_generator' in scope:
+            self.gui_actions = [
+                ('Next batch', self._next_batch),
+            ]
+
+    @functools.cached_property
+    def data_source(self):
+        if 'x_train' in self.scope:
+            return self.scope['x_train'], self.scope['y_train']
+        return next(self.scope['train_generator'])
+
+    def _next_batch(self, scope, storage):
+        self.data_source = next(self.scope['train_generator'])
+        if scope.get('rerun_callback', None) is not None:
+            scope['rerun_callback']()
