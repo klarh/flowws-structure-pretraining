e4cfebd90343e7884c9bbe2b752cb7f3cc22e60c
Matthew Spellings <mspells@vectorinstitute.ai>
2022-02-16 13:07:02 -0500

    Add explained variance plot for PCAEmbedding

--- flowws_structure_pretraining/analysis/PCAEmbedding.py
+++ flowws_structure_pretraining/analysis/PCAEmbedding.py
@@ -1,5 +1,6 @@
 import flowws
 from flowws import Argument as Arg
+import numpy as np
 from sklearn.decomposition import PCA
 
 
@@ -9,11 +10,20 @@ class PCAEmbedding(flowws.Stage):
     """Use PCA to project the embedding"""
 
     ARGS = [
-        Arg('n_dim', '-d', int, 2, help='Target dimensionality'),
+        Arg('n_dim', '-d', int, 8, help='Target dimensionality'),
     ]
 
     def run(self, scope, storage):
         x = scope['embedding']
-        pca = scope['pca'] = PCA(self.arguments['n_dim'])
-        y = pca.fit_transform(x)
+        self.pca = scope['pca'] = PCA(self.arguments['n_dim'])
+        y = self.pca.fit_transform(x)
         scope['embedding_pca'] = y
+        scope.setdefault('visuals', []).append(self)
+
+    def draw_matplotlib(self, fig):
+        ax = fig.add_subplot()
+        x = 1 + np.arange(self.pca.n_components)
+        y = np.cumsum(self.pca.explained_variance_ratio_)
+        ax.plot(x, y)
+        ax.set_xlabel('Components')
+        ax.set_ylabel('Explained variance')
