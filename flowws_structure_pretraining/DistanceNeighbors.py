ed00697fddee42f032926f87ffa2da5ade37e2ba
Matthew Spellings <mspells@vectorinstitute.ai>
2022-02-18 12:17:42 -0500

    Add rdf_shells argument to DistanceNeighbors

--- flowws_structure_pretraining/DistanceNeighbors.py
+++ flowws_structure_pretraining/DistanceNeighbors.py
@@ -1,3 +1,5 @@
+import itertools
+
 import flowws
 from flowws import Argument as Arg
 import freud
@@ -14,6 +16,20 @@ class DistanceNeighbors(flowws.Stage):
             'max_neighbors', '-n', int, 16, help='Maximum number of neighbors to allow'
         ),
         Arg('r_cut', '-r', float, 2, help='Cutoff distance to use'),
+        Arg(
+            'rdf_shells',
+            None,
+            int,
+            help='Calculate cutoff distance based on the given shell number in the RDF',
+        ),
+        Arg('rdf_distance', None, float, 0.1, help='Bin size to use for RDFs'),
+        Arg(
+            'rdf_smoothing',
+            None,
+            int,
+            8,
+            help='Smoothing lengthscale to apply for RDFs',
+        ),
     ]
 
     def run(self, scope, storage):
@@ -22,11 +38,52 @@ class DistanceNeighbors(flowws.Stage):
         scope['max_neighbors'] = self.arguments['max_neighbors']
 
     def get_nlist(self, box, positions):
+        rcut = self.arguments['r_cut']
+        if 'rdf_shells' in self.arguments:
+            done = False
+            rdf_rmax = 2 * self.arguments['r_cut']
+            while not done:
+                rdf = freud.density.RDF(
+                    int(rdf_rmax / self.arguments['rdf_distance']), rdf_rmax
+                )
+                rdf.compute((box, positions))
+
+                r = rdf.bin_centers
+                y = rdf.rdf
+                dr = np.diff(rdf.bounds) / rdf.nbins
+                v = np.diff(np.cumsum(r * dr * y))
+                dist = np.argmax(np.convolve(v, v.conj(), mode='same'))
+                dist //= self.arguments['rdf_smoothing']
+                filt = np.full(dist, 1.0 / dist)
+                v = np.convolve(v, filt, mode='same')
+                d = np.diff(v)
+                sd = np.diff(np.sign(d))
+                maxima = set(np.where(sd < -0.5)[0] + 1)
+                minima = set(np.where(sd > 0.5)[0] + 1)
+                extrema = list(sorted(maxima.union(minima)))
+                extrema_groups = itertools.groupby(extrema, lambda x: x in maxima)
+
+                take_minimum = False
+                shells = []
+                for (in_maxima, group) in extrema_groups:
+                    if in_maxima:
+                        take_minimum = True
+                    elif take_minimum:
+                        index = int(np.median(list(group)))
+                        shells.append(index)
+                        take_minimum = False
+
+                if len(shells) > self.arguments['rdf_shells']:
+                    index = shells[self.arguments['rdf_shells'] - 1]
+                    rcut = r[index]
+                    done = rdf.nbins - index > self.arguments['rdf_smoothing']
+
+                if not done:
+                    rdf_rmax *= 1.25
+
         q = freud.locality.AABBQuery(box, positions)
         maximum_neighbors = self.arguments['max_neighbors']
-        qr = q.query(
-            positions, dict(mode='ball', r_max=self.arguments['r_cut'], exclude_ii=True)
-        )
+        qr = q.query(positions, dict(mode='ball', r_max=rcut, exclude_ii=True))
         nl = qr.toNeighborList(sort_by_distance=False)
 
         if np.any(nl.neighbor_counts > maximum_neighbors):
