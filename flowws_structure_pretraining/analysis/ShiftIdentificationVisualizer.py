d54103d30df98ed846768d6e0ed851edcd53bfcc
Matthew Spellings <mspells@vectorinstitute.ai>
2022-04-29 11:44:32 -0400

    Add draw_shift and width arguments to ShiftIdentificationVisualizer

--- flowws_structure_pretraining/analysis/ShiftIdentificationVisualizer.py
+++ flowws_structure_pretraining/analysis/ShiftIdentificationVisualizer.py
@@ -34,6 +34,16 @@ class ShiftIdentificationVisualizer(flowws.Stage, GeneratorVisualizer):
         Arg('color_min', None, float, 0.0, help='Minimum colormap value'),
         Arg('color_max', None, float, 1.0, help='Maximum colormap value'),
         Arg('contrast', None, float, 1.0, help='Contrast scale'),
+        Arg('draw_shift', None, bool, False,
+            help='Render shifts as lines'),
+        Arg(
+            'width',
+            None,
+            float,
+            0.25,
+            valid_values=flowws.Range(0, 2.0, True),
+            help='Bond rendering width',
+        ),
     ]
 
     Frame = FileLoader.Frame
@@ -41,14 +51,31 @@ class ShiftIdentificationVisualizer(flowws.Stage, GeneratorVisualizer):
     def run(self, scope, storage):
         self.scope = scope
         (positions, starts, ends, theta) = self.get_predictions(scope['cache_key'])
-        theta = self.remap_theta(theta)
+        if self.arguments['draw_shift']:
+            starts = scope['position']
+            ends = starts + self.last_prediction
+            theta = self.remap_theta(theta)
 
-        colors = plato.cmap.cubehelix(theta)
-        colors[:, :3] *= self.arguments['color_scale']
-        scope['color'] = colors
+            colors = plato.cmap.cubehelix(theta)
+            colors[:, :3] *= self.arguments['color_scale']
+            prim = draw.Lines(
+                start_points=starts,
+                end_points=ends,
+                widths=np.full(len(theta), self.arguments['width']),
+                colors=colors,
+                cap_mode=1,
+            )
 
-    @staticmethod
-    def theta_prediction(prediction):
+            scope.setdefault('plato_primitives', []).append(prim)
+        else:
+            theta = self.remap_theta(theta)
+
+            colors = plato.cmap.cubehelix(theta)
+            colors[:, :3] *= self.arguments['color_scale']
+            scope['color'] = colors
+
+    def theta_prediction(self, prediction):
+        self.last_prediction = prediction.theta
         return prediction._replace(theta=np.linalg.norm(prediction.theta, axis=-1))
 
     @staticmethod
