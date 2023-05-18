cf2da0fa819378b6ad9f1241d76ee5277a6a5239
Matthew Spellings <mspells@vectorinstitute.ai>
2023-05-18 09:40:08 -0400

    Make FrameRegressionTask able to use arbitrary labels from the context dict

--- flowws_structure_pretraining/tasks/FrameRegressionTask.py
+++ flowws_structure_pretraining/tasks/FrameRegressionTask.py
@@ -9,26 +9,47 @@ from .FrameClassificationTask import FrameClassificationTask
 class FrameRegressionTask(FrameClassificationTask):
     """Generate training data to identify from which frame a sample came, as a continuous quantity"""
 
-    ARGS = [
+    ARGS = FrameClassificationTask.ARGS + [
         Arg(
-            'x_scale', '-x', float, 2.0, help='Scale by which to divide input distances'
+            'context_label',
+            None,
+            str,
+            help='If given, use this value as the key to extract labels from context dictionaries',
         ),
-        Arg('seed', '-s', int, 13, help='RNG seed for data generation'),
-        Arg('subsample', None, float, help='Take only the given fraction of data'),
-        Arg('shuffle', None, bool, True, help='If True, shuffle data'),
         Arg(
-            'per_cloud',
-            '-p',
+            'normalize_labels',
+            None,
             bool,
             False,
-            help='If True, classify clouds rather than individual bonds',
+            help='If True, normalize labels to have a mean of 0 and variance of 1',
         ),
     ]
 
     def run(self, scope, storage):
         super().run(scope, storage)
-        class_remap = np.linspace(0, 1, len(scope['label_remap']))
-        scope['y_train'] = class_remap[scope['y_train'][..., 0]]
+        if 'context_label' in self.arguments:
+            key = self.arguments['context_label']
+            ys = [ctx[key] for ctx in scope['x_contexts']]
+        else:
+            class_remap = np.linspace(0, 1, len(scope['label_remap']))
+            ys = class_remap[scope['y_train'][..., 0]]
+
+        if self.arguments['normalize_labels']:
+            from flowws_keras_geometry.data.internal import ScaledMAE
+
+            mu, sigma = np.mean(ys), np.std(ys)
+            ys = (np.array(ys) - mu) / sigma
+            if 'validation_data' in scope:
+                scope['validation_data'][-1] -= mu
+                scope['validation_data'][-1] /= sigma
+            scope['normalize_label_mean'] = mu
+            scope['normalize_label_std'] = sigma
+
+            metrics = scope.setdefault('metrics', [])
+            scaled_mae = ScaledMAE(sigma, name='scaled_mae')
+            metrics.append(scaled_mae)
+
+        scope['y_train'] = np.asarray(ys)
         scope['loss'] = 'mse'
         scope['metrics'].remove('accuracy')
         scope['metrics'].append('mean_absolute_error')
