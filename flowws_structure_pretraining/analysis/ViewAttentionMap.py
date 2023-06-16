afb2c1d3ea5a8d2a60d1d6718a47ac571e7f8d77
Matthew Spellings <mspells@vectorinstitute.ai>
2023-06-16 14:27:55 -0400

    Improve visualization arguments in ViewAttentionMap

--- flowws_structure_pretraining/analysis/ViewAttentionMap.py
+++ flowws_structure_pretraining/analysis/ViewAttentionMap.py
@@ -28,6 +28,24 @@ class ViewAttentionMap(flowws.Stage):
             help='Attention layer index to use (starting with the final layer)',
         ),
         Arg('center', '-c', bool, True, help='If True, center particles'),
+        Arg('additive_rendering', None, bool, True, help='Utilize additive rendering'),
+        Arg(
+            'fast_antialiasing',
+            None,
+            bool,
+            False,
+            help='Use Fast Approximate Antialiasing (FXAA)',
+        ),
+        Arg(
+            'color_scale',
+            None,
+            float,
+            1,
+            valid_values=flowws.Range(0, 10, True),
+            help='Factor to scale color RGB intensities by',
+        ),
+        Arg('draw_scale', '-s', float, 1, help='Scale to multiply particle size by'),
+        Arg('display_box', '-b', bool, True, help='Display the system box'),
     ]
 
     def run(self, scope, storage):
@@ -51,6 +69,7 @@ class ViewAttentionMap(flowws.Stage):
             diameters = scope['diameter']
         except KeyError:
             diameters = 1.0
+        diameters *= self.arguments['draw_scale']
 
         try:
             fbox = freud.box.Box.from_box(scope['box'])
@@ -68,18 +87,22 @@ class ViewAttentionMap(flowws.Stage):
             positions -= com
             positions = fbox.wrap(positions)
 
-        colors = np.ones((len(types), 4)) * 0.25
+        colors = np.ones((len(types), 4))
         colors[:, :3] = plato.cmap.cubeellipse_intensity(types.astype(np.float32))
+        colors[:, :3] *= self.arguments['color_scale']
 
         prims = []
+        scene_kwargs = {}
 
         if not self.per_molecule:
             try:
                 center_diam = scope['center_diameter']
+                gray = np.mean(colors[:, :3])
+                gray = (gray, gray, gray, 1)
 
                 centerprim = draw.Spheres(
                     positions=[-com],
-                    colors=[(0.5, 0.5, 0.5, 1)],
+                    colors=[gray],
                     diameters=center_diam,
                 )
                 prims.append(centerprim)
@@ -94,19 +117,39 @@ class ViewAttentionMap(flowws.Stage):
         edge_ends = positions[ej]
         if fbox is not None:
             edge_ends = edge_starts + fbox.wrap(edge_ends - edge_starts)
-        edge_colors = np.ones((len(edge_starts), 4)) * 0.5
-        edge_colors[:, :3] *= att[ei, ej, None]
+        edge_colors = np.ones((len(edge_starts), 4))
+        edge_colors[:, :3] *= att[ei, ej, None] * np.mean(colors[:, :3])
         edgeprim = draw.Lines(
             start_points=edge_starts,
             end_points=edge_ends,
             colors=edge_colors,
             widths=0.125,
+            cap_mode=1,
         )
         prims.append(edgeprim)
-        colors[:, :3] *= np.diag(att)[:, None] / np.max(np.diag(att))
+        colors[:, :3] *= np.diag(att)[:, None]
         prim.colors = colors
 
-        self.scene = draw.Scene(prims, features=dict(additive_rendering=True))
+        if 'box' in scope and self.arguments['display_box']:
+            boxprim = draw.Box.from_box(scope['box'])
+            boxprim.width = min(scope['box'][:3]) * 5e-3
+            gray = np.mean(colors[:, :3])
+            boxprim.color = (gray, gray, gray, 1)
+            prims.append(boxprim)
+
+            scene_kwargs['size'] = 1.05 * np.array(scope['box'][:2])
+            # use default size of 800px wide
+            scene_kwargs['pixel_scale'] = 800 / scene_kwargs['size'][0]
+
+        features = {}
+
+        if self.arguments['additive_rendering']:
+            features['additive_rendering'] = True
+
+        if self.arguments['fast_antialiasing']:
+            features['fxaa'] = True
+
+        self.scene = draw.Scene(prims, features=features, **scene_kwargs)
 
         scope.setdefault('visuals', []).append(self)
 
