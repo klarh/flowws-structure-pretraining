72900eb39c4ab460887c4fa7858ad5d03fb60067
Matthew Spellings <matthew.p.spellings@gmail.com>
2023-11-14 20:06:53 -0500

    Make task modules usable without keras/tensorflow

--- flowws_structure_pretraining/__init__.py
+++ flowws_structure_pretraining/__init__.py
@@ -1,17 +1,25 @@
+from flowws import try_to_import
+
 from .version import __version__
 
-from .ClearMetrics import ClearMetrics
-from .ContextMapper import ContextMapper
-from .ContextSplitDataset import ContextSplitDataset
-from .DistanceNeighbors import DistanceNeighbors
-from .FileLoader import FileLoader
-from .Frame2Molecule import Frame2Molecule
-from .FrameFilter import FrameFilter
-from .LimitAccuracyCallback import LimitAccuracyCallback
-from .LoadModel import LoadModel
-from .NearestNeighbors import NearestNeighbors
-from .NormalizeNeighborDistance import NormalizeNeighborDistance
-from .PyriodicLoader import PyriodicLoader
-from .SANNeighbors import SANNeighbors
-from .SplitDataset import SplitDataset
-from .VoronoiNeighbors import VoronoiNeighbors
+ClearMetrics = try_to_import('.ClearMetrics', 'ClearMetrics', __name__)
+ContextMapper = try_to_import('.ContextMapper', 'ContextMapper', __name__)
+ContextSplitDataset = try_to_import(
+    '.ContextSplitDataset', 'ContextSplitDataset', __name__
+)
+DistanceNeighbors = try_to_import('.DistanceNeighbors', 'DistanceNeighbors', __name__)
+FileLoader = try_to_import('.FileLoader', 'FileLoader', __name__)
+Frame2Molecule = try_to_import('.Frame2Molecule', 'Frame2Molecule', __name__)
+FrameFilter = try_to_import('.FrameFilter', 'FrameFilter', __name__)
+LimitAccuracyCallback = try_to_import(
+    '.LimitAccuracyCallback', 'LimitAccuracyCallback', __name__
+)
+LoadModel = try_to_import('.LoadModel', 'LoadModel', __name__)
+NearestNeighbors = try_to_import('.NearestNeighbors', 'NearestNeighbors', __name__)
+NormalizeNeighborDistance = try_to_import(
+    '.NormalizeNeighborDistance', 'NormalizeNeighborDistance', __name__
+)
+PyriodicLoader = try_to_import('.PyriodicLoader', 'PyriodicLoader', __name__)
+SANNeighbors = try_to_import('.SANNeighbors', 'SANNeighbors', __name__)
+SplitDataset = try_to_import('.SplitDataset', 'SplitDataset', __name__)
+VoronoiNeighbors = try_to_import('.VoronoiNeighbors', 'VoronoiNeighbors', __name__)
--- flowws_structure_pretraining/tasks/__init__.py
+++ flowws_structure_pretraining/tasks/__init__.py
@@ -1,15 +1,37 @@
-from .AutoencoderTask import AutoencoderTask
-from .AutoencoderTaskTransformer import AutoencoderTaskTransformer
-from .DenoisingTask import DenoisingTask
-from .DenoisingTaskTransformer import DenoisingTaskTransformer
-from .FrameClassificationTask import FrameClassificationTask
-from .FrameRegressionTask import FrameRegressionTask
-from .FrameRegressionTaskTransformer import FrameRegressionTaskTransformer
-from .NearBondTask import NearBondTask
-from .NearBondTaskTransformer import NearBondTaskTransformer
-from .NearestBondTask import NearestBondTask
-from .NoisyBondTask import NoisyBondTask
-from .NoisyBondTaskTransformer import NoisyBondTaskTransformer
-from .PointGroupTask import PointGroupTask
-from .ShiftIdentificationTask import ShiftIdentificationTask
-from .ShiftIdentificationTaskTransformer import ShiftIdentificationTaskTransformer
+from flowws import try_to_import
+
+AutoencoderTask = try_to_import('.AutoencoderTask', 'AutoencoderTask', __name__)
+AutoencoderTaskTransformer = try_to_import(
+    '.AutoencoderTaskTransformer', 'AutoencoderTaskTransformer', __name__
+)
+DenoisingTask = try_to_import('.DenoisingTask', 'DenoisingTask', __name__)
+DenoisingTaskTransformer = try_to_import(
+    '.DenoisingTaskTransformer', 'DenoisingTaskTransformer', __name__
+)
+FrameClassificationTask = try_to_import(
+    '.FrameClassificationTask', 'FrameClassificationTask', __name__
+)
+FrameRegressionTask = try_to_import(
+    '.FrameRegressionTask', 'FrameRegressionTask', __name__
+)
+FrameRegressionTaskTransformer = try_to_import(
+    '.FrameRegressionTaskTransformer', 'FrameRegressionTaskTransformer', __name__
+)
+NearBondTask = try_to_import('.NearBondTask', 'NearBondTask', __name__)
+NearBondTaskTransformer = try_to_import(
+    '.NearBondTaskTransformer', 'NearBondTaskTransformer', __name__
+)
+NearestBondTask = try_to_import('.NearestBondTask', 'NearestBondTask', __name__)
+NoisyBondTask = try_to_import('.NoisyBondTask', 'NoisyBondTask', __name__)
+NoisyBondTaskTransformer = try_to_import(
+    '.NoisyBondTaskTransformer', 'NoisyBondTaskTransformer', __name__
+)
+PointGroupTask = try_to_import('.PointGroupTask', 'PointGroupTask', __name__)
+ShiftIdentificationTask = try_to_import(
+    '.ShiftIdentificationTask', 'ShiftIdentificationTask', __name__
+)
+ShiftIdentificationTaskTransformer = try_to_import(
+    '.ShiftIdentificationTaskTransformer',
+    'ShiftIdentificationTaskTransformer',
+    __name__,
+)
